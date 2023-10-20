21c0f54c93d6f94b563db0a497b4704dded7816e
Willy Dabou <dabouwilly@gmail.com>
2023-10-20 11:55:33 +0300

    v03

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -2,7 +2,7 @@ import CV_fr from './CV_fr.pdf'
 export const Resume = () =>{
     return(
         <div className="md:mx-16 mx-8 my-6" > 
-            <h3 className="text-xl" >Resume</h3>
+            <h3 className="text-xl" >Resume (CV)</h3>
             <a href={CV_fr} >
                 <button className="bg-blue-400 rounded-md my-2 p-2 text-white font-bold ">Download</button>
             </a>
